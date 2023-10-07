bcd095a91f141879998e6d3b8861b17cc06d954a
Mohammed Al-Tabaei <117546290+42778312@users.noreply.github.com>
2023-10-08 00:30:25 +0200

    Update Program.cs

--- Einheiten umrechnen/Program.cs
+++ Einheiten umrechnen/Program.cs
@@ -6,12 +6,12 @@ namespace UnitConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Einheiten-Umrechner App ");
+            Console.WriteLine("Willkommen zum Einheiten-Umrechner!");
 
             while (true)
             {
-                Console.WriteLine("\n Welche Rechnungsart wollen Sie rechnen:");
-                Console.WriteLine("\n1. Temperatur umrechnen");
+                Console.WriteLine("\nBitte wählen Sie die Art der Umrechnung:");
+                Console.WriteLine("1. Temperatur umrechnen");
                 Console.WriteLine("2. Masseinheiten umrechnen");
                 Console.WriteLine("3. Gewichtseinheiten umrechnen");
                 Console.WriteLine("4. Beenden");
@@ -20,7 +20,7 @@ namespace UnitConverter
 
                 if (choice == 4)
                 {
-                    Console.WriteLine("Vielen Dank.Tschüsse ");
+                    Console.WriteLine("Vielen Dank für die Verwendung des Einheiten-Umrechners. Auf Wiedersehen!");
                     break;
                 }
 
@@ -40,49 +40,49 @@ namespace UnitConverter
                         break;
                 }
             }
-        } // the beginning of the code and 
+        }
 
         static void ConvertTemperature()
         {
-            Console.WriteLine("\nBitte wählen Sie die Quelleinheit der Temperatur:");
-            Console.WriteLine("\n1. Celsius");
+            Console.WriteLine("\nBitte wählen Sie die Ausgangseinheit der Temperatur:");
+            Console.WriteLine("1. Celsius");
             Console.WriteLine("2. Fahrenheit");
             Console.WriteLine("3. Kelvin");
 
-            int quelle = int.Parse(Console.ReadLine());
+            int sourceUnit = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nBitte wählen Sie die Zieleinheit der Temperatur:");
-            Console.WriteLine("\n1. Celsius");
+            Console.WriteLine("\nBitte wählen Sie die Ziel-Einheit der Temperatur:");
+            Console.WriteLine("1. Celsius");
             Console.WriteLine("2. Fahrenheit");
             Console.WriteLine("3. Kelvin");
 
-            int ziel = int.Parse(Console.ReadLine());
+            int targetUnit = int.Parse(Console.ReadLine());
 
             Console.Write("Geben Sie den Temperaturwert ein: ");
             double temperature = double.Parse(Console.ReadLine());
             double result = 0;
 
-            if (quelle == 1 && ziel == 2)
+            if (sourceUnit == 1 && targetUnit == 2)
             {
                 result = CelsiusToFahrenheit(temperature);
             }
-            else if (quelle == 2 && ziel == 1)
+            else if (sourceUnit == 2 && targetUnit == 1)
             {
                 result = FahrenheitToCelsius(temperature);
             }
-            else if (quelle == 1 && ziel == 3)
+            else if (sourceUnit == 1 && targetUnit == 3)
             {
                 result = CelsiusToKelvin(temperature);
             }
-            else if (quelle == 3 && ziel == 1)
+            else if (sourceUnit == 3 && targetUnit == 1)
             {
                 result = KelvinToCelsius(temperature);
             }
-            else if (quelle == 2 && ziel == 3)
+            else if (sourceUnit == 2 && targetUnit == 3)
             {
                 result = FahrenheitToKelvin(temperature);
             }
-            else if (quelle == 3 && ziel == 2)
+            else if (sourceUnit == 3 && targetUnit == 2)
             {
                 result = KelvinToFahrenheit(temperature);
             }
@@ -92,96 +92,123 @@ namespace UnitConverter
                 return;
             }
 
-            Console.WriteLine($"Ergebnis: {result} {unitName(ziel)}");
-        }  quelle Einheit und result 
-        //_________________________________________________________________________________________________________
+            Console.WriteLine($"Ergebnis: {temperature} Einheit {GetUnitName(sourceUnit)} entsprechen {result} Einheit {GetUnitName(targetUnit)}");
+        }
+
         static void ConvertMassUnits()
-        //{
-        //    Console.WriteLine("\nBitte wählen Sie die Quelleinheit der Mass:");
-        //    Console.WriteLine("\n1. Kilometer");
-        //    Console.WriteLine("2. Meter");
-        //    Console.WriteLine("3. Zentimeter");
-            
+        {
+            Console.WriteLine("\nBitte wählen Sie die Ausgangseinheit der Masse:");
+            Console.WriteLine("1. Ton");
+            Console.WriteLine("2. Kilogramm");
+            Console.WriteLine("3. Gramm");
 
-        //    int quelleM = int.Parse(Console.ReadLine());
+            int sourceUnit = int.Parse(Console.ReadLine());
 
-        //    Console.WriteLine("\nBitte wählen Sie die Zieleinheit der Mass:");
-        //    Console.WriteLine("\n1. Kilometer");
-        //    Console.WriteLine("2. Meter");
-        //    Console.WriteLine("3. Zentimeter");
+            Console.WriteLine("\nBitte wählen Sie die Ziel-Einheit der Masse:");
+            Console.WriteLine("1. Ton");
+            Console.WriteLine("2. Kilogramm");
+            Console.WriteLine("3. Gramm");
 
-        //    int zielM = int.Parse(Console.ReadLine());
+            int targetUnit = int.Parse(Console.ReadLine());
 
-        //    Console.Write("Geben Sie einen Wert ein: ");
-        //    double mass = double.Parse(Console.ReadLine());
-        //    double result = 0;
+            Console.Write("Geben Sie den Massenwert ein: ");
+            double mass = double.Parse(Console.ReadLine());
+            double result = 0;
 
-        //    if (quelleM == 1 && zielM == 2)
-        //    {
-        //        result = kilometerToMeter(mass);
-        //    }
-        //    else if (quelleM == 2 && zielM == 1)
-        //    {
-        //        result = meterToKilometer(mass);
-        //    }
-        //    else if (quelleM == 1 && zielM == 3)
-        //    {
-        //        result = kilometerToZentimeter(mass);
-        //    }
-        //    else if (quelleM == 3 && zielM == 1)
-        //    {
-        //        result = zentimeterToKilometer(mass);
-        //    }
-        //    else if (quelleM == 2 && zielM == 3)
-        //    {
-        //        result = meterToZentimeter(mass);
-        //    }
-        //    else if (quelleM == 3 && zielM == 2)
-        //    {
-        //        result = zentimeterToMeter(mass);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Ungültige Auswahl der Einheiten.");
-        //        return;
-        //    }
+            if (sourceUnit == 1 && targetUnit == 2)
+            {
+                result = TonToKilogram(mass);
+            }
+            else if (sourceUnit == 2 && targetUnit == 1)
+            {
+                result = KilogramToTon(mass);
+            }
+            else if (sourceUnit == 1 && targetUnit == 3)
+            {
+                result = TonToGram(mass);
+            }
+            else if (sourceUnit == 3 && targetUnit == 1)
+            {
+                result = GramToTon(mass);
+            }
+            else if (sourceUnit == 2 && targetUnit == 3)
+            {
+                result = KilogramToGram(mass);
+            }
+            else if (sourceUnit == 3 && targetUnit == 2)
+            {
+                result = GramToKilogram(mass);
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Auswahl der Einheiten.");
+                return;
+            }
 
-        //    Console.WriteLine($"Ergebnis: {result} {unitName(ziel)}");
-        //} // quelle Einheit und result 
-    }//____________________________________________________________________________________________________________
+            Console.WriteLine($"Ergebnis: {mass} Einheit {GetUnitName(sourceUnit)} entsprechen {result} Einheit {GetUnitName(targetUnit)}");
+        }
 
         static void ConvertWeightUnits()
         {
-            // Implementiere die Umrechnung von Gewichtseinheiten hier
-            // Ähnlich wie bei der Temperaturumrechnung
+            Console.WriteLine("\nBitte wählen Sie die Ausgangseinheit der Gewichtseinheit:");
+            Console.WriteLine("1. Pfund");
+            Console.WriteLine("2. Kilogramm");
+            // Add more weight units as needed
+
+            int sourceUnit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nBitte wählen Sie die Ziel-Einheit der Gewichtseinheit:");
+            Console.WriteLine("1. Pfund");
+            Console.WriteLine("2. Kilogramm");
+            // Add more weight units as needed
+
+            int targetUnit = int.Parse(Console.ReadLine());
+
+            Console.Write("Geben Sie den Gewichtswert ein: ");
+            double weight = double.Parse(Console.ReadLine());
+            double result = 0;
+
+            if (sourceUnit == 1 && targetUnit == 2)
+            {
+                result = PoundsToKilograms(weight);
+            }
+            else if (sourceUnit == 2 && targetUnit == 1)
+            {
+                result = KilogramsToPounds(weight);
+            }
+            // Add more weight unit conversions as needed
+
+            Console.WriteLine($"Ergebnis: {weight} Einheit {GetUnitName(sourceUnit)} entsprechen {result} Einheit {GetUnitName(targetUnit)}");
         }
-    //_____________________________________________________________________________________________________________
-        static string unitName(int unit)
+
+        static string GetUnitName(int unit)
         {
             switch (unit)
             {
                 case 1:
+                    return "Ton";
+                case 2:
+                    return "Kilogramm";
+                case 3:
+                    return "Gramm";
+                case 4:
                     return "Celsius";
-                case 2:
+                case 5:
                     return "Fahrenheit";
-                case 3:
+                case 6:
                     return "Kelvin";
-                case 1:
-                return "Kilometer";
-                case 2:
-                return "meter";
-                case 3:
-                return "Zentimeter";
-            // Füge hier weitere Einheiten hinzu
-            default:
+                case 7:
+                    return "Pfund";
+                // Add more unit names as needed
+                default:
                     return "Unbekannt";
             }
-        } // Einheiten aufrufen und im Result hinzufügen 
-    //_____________________________________________________________________________________________________________
+        }
+
         static double CelsiusToFahrenheit(double celsius)
         {
             return (celsius * 9 / 5) + 32;
-        }   // einheiten rechnungen , die direkt zur ConvertTemperature klasse hinzufügen sollten
+        }
 
         static double FahrenheitToCelsius(double fahrenheit)
         {
@@ -207,32 +234,48 @@ namespace UnitConverter
         {
             return (kelvin - 273.15) * 9 / 5 + 32;
         }
-    //___________________________________________________________________________________
-    //static double kilometerToMeter(double kilometer)
-    //{
-    //    return kilometer * 1000;
-    //}
-    //static double meterToKilometer(double kelvin)
-    //{
-    //    return kilometer / 1000; 
-    //}
-    //static double kilometerToZentimeter(double kilometer)
-    //{
-    //    return kilometer * 100000;
-    //}
-    //static double zentimeterToKilometer(double zentimeter)
-    //{
-    //    return zentimeter / 100000;
-    //}
-    //static double meterToZentimeter(double meter)
-    //{
-    //    return meter *100;
-    //}
-    //static double zentimeterToMeter(double zentimeter)
-    //{
-    //    return zentimeter  / 100;
-    //}
-    //____________________________________________________________________________________
 
+        static double TonToKilogram(double ton)
+        {
+            return ton * 1000;
+        }
+
+        static double KilogramToTon(double kilogram)
+        {
+            return kilogram / 1000;
+        }
+
+        static double TonToGram(double ton)
+        {
+            return ton * 1_000_000;
+        }
+
+        static double GramToTon(double grams)
+        {
+            return grams / 1_000_000;
+        }
+
+        static double KilogramToGram(double kilogram)
+        {
+            return kilogram * 1000;
+        }
+
+        static double GramToKilogram(double grams)
+        {
+            return grams / 1000;
+        }
+
+        static double PoundsToKilograms(double pounds)
+        {
+            return pounds * 0.453592;
+        }
+
+        static double KilogramsToPounds(double kilograms)
+        {
+            return kilograms / 0.453592;
+        }
+    }
 }
-}
+
+
+//The Code work but it shows some errors in Gewicht einheit result . 
